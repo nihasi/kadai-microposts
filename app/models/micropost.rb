c16f6cc902fbc1f9680bce3db682b365fb9eff06
inawa <g16a54jbf@yahoo.co.jp>
2018-09-20 01:19:15 +0000

    add Favorite function

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  has_many :likes, dependent: :destroy
+  has_many :favorited, through: :likes, source: :user
   
   validates:user_id, presence:true
   validates:content, presence:true, length:{maximum:255}
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,9 @@ class User < ApplicationRecord
     has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
     has_many :followers, through: :reverses_of_relationship, source: :user
     
+    has_many :likes
+    has_many :favorite, through: :likes, source: :micropost
+    
     def follow(other_user)
         unless self == other_user
           self.relationships.find_or_create_by(follow_id: other_user.id)
@@ -30,5 +33,18 @@ class User < ApplicationRecord
     
     def feed_microposts
        Micropost.where(user_id:self.following_ids + [self.id]) 
+    end
+    
+    def like(micropost)
+        self.likes.find_or_create_by(micropost_id:micropost.id)
+    end
+    
+    def unlike(micropost)
+        like=self.likes.find_by(micropost_id:micropost.id)
+        like.destroy if like
+    end
+    
+    def like?(micropost)
+        self.favorite.include?(micropost)
     end
 end
